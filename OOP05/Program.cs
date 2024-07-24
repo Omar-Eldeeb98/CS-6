a077de5faa3efb92355b050759152fda20f654fd
Omar-Eldeeb98 <oeldeeb111@gmail.com>
2024-07-24 20:53:03 +0300

    assignment solution

---
+++ OOP05/Program.cs
@@ -0,0 +1,201 @@
+﻿using OOP05.First_Project;
+using OOP05.Second_Project;
+using OOP05.Third_Project;
+
+namespace OOP05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region First Project
+
+            #region 2
+            //Point3D p = new Point3D(10, 20, 30);
+            //Console.WriteLine(p.ToString());  // Point Coordinates: (10 , 20 , 30)
+            #endregion
+
+
+            #region 3
+            //Console.WriteLine("enter coordinates for point 1");
+            //Point3D p1 = Point3D.ReadPoint();
+
+
+            //Console.WriteLine("\n");
+
+            //Console.WriteLine("enter coordinates for point 2");
+            //Point3D p2 = Point3D.ReadPoint();
+
+
+            //Console.WriteLine("\n");
+
+            //Console.WriteLine("----------------------------------");
+
+            //Console.WriteLine($"P1: {p1}");
+            //Console.WriteLine($"P2: {p2}");
+
+
+            //Console.WriteLine("\n");
+
+            #endregion
+
+
+            #region 4 
+            //Console.WriteLine(p1 == p2);
+            // Point Coordinates: (1 , 2 , 3) = Point Coordinates: (1 , 2 , 3)
+            // True
+
+            #endregion
+
+
+            #region 5 
+
+            //Console.WriteLine("enter the numbers of Points:");
+            //int numsOfPoints;
+            //while (!int.TryParse(Console.ReadLine(), out numsOfPoints) || numsOfPoints <= 0)
+            //{
+            //    Console.WriteLine("Invalid number. Please enter integer number");
+            //}
+            //Point3D[] arrayPoints = new Point3D[numsOfPoints];
+
+            //for (int i = 0; i < numsOfPoints; i++)
+            //{
+            //    Console.WriteLine($"enter coordinates for point: {i + 1}");
+            //    arrayPoints[i] = Point3D._ReadPoint();
+            //}
+
+
+            //Array.Sort(arrayPoints);
+
+            //Console.WriteLine("points after sort by X and Y coordinates:");
+
+            //foreach (Point3D point in arrayPoints)
+            //{
+            //    Console.WriteLine(point);
+            //}
+            #endregion
+
+
+            #region 6
+
+            //Console.WriteLine("\n");
+
+            //Point3D[] p1 = new Point3D[2]
+            //{
+            //    new Point3D(1,2,3),
+            //    new Point3D(4,5,6)
+            //};
+
+
+
+            //Point3D[] cloneP = (Point3D[])p1.Clone();
+
+            //Console.WriteLine(p1[1]);    // Point Coordinates: (4 , 5 , 6)
+            //Console.WriteLine(cloneP[1]);// Point Coordinates: (4 , 5 , 6)
+
+            #endregion
+
+
+
+
+
+            #endregion
+
+
+            #region Second Project
+
+
+            //int a = 10;
+            //int b = 5;
+
+            //Console.WriteLine("Add: " + Maths.Add(a, b));              // Add: 15
+            //Console.WriteLine("Subtract: " + Maths.Subtract(a, b));    // Subtract: 5
+            //Console.WriteLine("Multiply: " + Maths.Multiply(a, b));    // Multiply: 50
+            //Console.WriteLine("Divide: " + Maths.Divide(a, b));        // Divide: 2
+
+
+            #endregion
+
+
+
+            #region Third Project
+
+            //Duration D1 = new Duration(1, 10, 15);
+            //Console.WriteLine(D1.ToString());  // Hours: 1 , Minutes: 10 , Seconds: 15
+            //Console.WriteLine("\n");
+
+            //Duration D2 = new Duration(3600);
+            //Console.WriteLine(D2.ToString()); // Hours: 1 , Minutes: 0 , Seconds: 0
+            //Console.WriteLine("\n");
+
+
+
+            //Duration D3 = new Duration(7800);
+            //Console.WriteLine(D3.ToString());  // Hours: 2 , Minutes: 10 , Seconds: 0
+            //Console.WriteLine("\n");
+
+
+
+            //Duration D4 = new Duration(666);
+            //Console.WriteLine(D4.ToString());   // Minutes: 11, Seconds: 6
+            //Console.WriteLine("\n");
+
+
+
+            //Duration Du3 = D1 + D2;
+            //Console.WriteLine(Du3.ToString()); // Hours: 2, Minutes: 10, Seconds: 15
+            //Console.WriteLine("\n");
+
+
+            //Duration Du3 = D1 +7800;
+            //Console.WriteLine(Du3.ToString()); // Hours: 3, Minutes: 20, Seconds: 1
+            //Console.WriteLine("\n");
+
+
+
+            //Duration Du3 = default;
+            //Du3 = Du3 + 666;
+            //Console.WriteLine(Du3.ToString()); // Hours: 1, Minutes: 10, Seconds: 15
+
+            //Duration Du3 = new Duration();
+            //Du3 = ++D1;
+            //Console.WriteLine(Du3.ToString()); // Hours: 1, Minutes: 11, Seconds: 15
+            //Console.WriteLine("\n");
+
+
+
+            //Duration Du3 = new Duration();
+            //Du3 = --D2;
+            //Console.WriteLine(Du3.ToString());   // Hours: 1, Minutes: -1, Seconds: 0
+            //Console.WriteLine("\n"); 
+
+
+            //D1 = D1 - D2;
+            //Console.WriteLine(D1.ToString());  // Minutes: 10, Seconds: 15
+            //Console.WriteLine("\n");
+
+
+            //Console.WriteLine(D1 > D2);  //True
+            //Console.WriteLine("\n");
+
+            //Console.WriteLine(D1 <= D2);  //False
+            //Console.WriteLine("\n");
+
+
+
+            //Console.WriteLine(D1); // True
+            //Console.WriteLine("\n");
+
+
+
+            //DateTime dt = (DateTime)D1;
+            //Console.WriteLine(dt);  
+            //Console.WriteLine("\n");
+
+
+
+            #endregion
+
+        }
+    }
+}
---
+++ OOP05/Third Project/Duration.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP05.Third_Project
+{
+    internal class Duration
+    {
+        // attributes
+        public int Hours { get; set; }
+        public int Minutes { get; set; }
+        public int Seconds { get; set; }
+
+
+        // constructors
+        public Duration(int hours, int minutes, int seconds)
+        {
+            Hours = hours;
+            Minutes = minutes;
+            Seconds = seconds;
+        }
+
+        // Parameterless constructor
+        public Duration()
+        {
+            Hours = 0;
+            Minutes = 0;
+            Seconds = 0;
+        }
+        // Constructor with total seconds
+        public Duration(int totalSeconds)
+        {
+            Hours = totalSeconds / 3600;
+            Minutes = (totalSeconds % 3600) / 60;
+            Seconds = totalSeconds % 60;
+        }
+
+
+        // methods
+
+        // Helper method to convert Duration to total seconds
+        private int ToTotalSeconds()
+        {
+            return Hours * 3600 + Minutes * 60 + Seconds;
+        }
+        public override string ToString()
+        {
+            if (Hours > 0)
+            {
+                return $"Hours: {Hours}, Minutes: {Minutes}, Seconds: {Seconds}";
+            }
+            else if (Minutes > 0)
+            {
+                return $"Minutes: {Minutes}, Seconds: {Seconds}";
+            }
+            else
+            {
+                return $"Seconds: {Seconds}";
+            }
+        }
+
+
+        public override bool Equals(object? obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            Duration d = (Duration)obj;
+            return Hours == d.Hours && Minutes == d.Minutes && Seconds == d.Seconds;
+        }
+
+        public override int GetHashCode()
+        {
+        
+            return Hours.GetHashCode() ^ Minutes.GetHashCode() ^ Seconds.GetHashCode();
+        }
+
+
+
+
+
+        // operators overloading
+       
+        // + overloading
+        public static Duration operator +(Duration d1, Duration d2)
+        {
+
+            return new Duration()
+            {
+                Hours = d1.Hours + d2.Hours,
+                Minutes = d1.Minutes + d2.Minutes,
+                Seconds = d1.Seconds + d2.Seconds
+            };    
+
+        }
+
+
+        // - overloading
+        public static Duration operator -(Duration d1, Duration d2)
+        {
+
+            return new Duration()
+            {
+                Hours = d1.Hours - d2.Hours,
+                Minutes = d1.Minutes - d2.Minutes,
+                Seconds = d1.Seconds - d2.Seconds
+            };
+
+        }
+
+
+
+        public static Duration operator +(Duration d, int totalSeconds)
+        {
+
+            return new Duration((d.ToTotalSeconds() + totalSeconds));
+            
+           
+        }
+
+
+        // operator (++) overloading method
+        public static Duration operator ++(Duration duration)
+        {
+            return new Duration()
+            {
+                Hours = duration.Hours,
+               Minutes = duration.Minutes + 1,
+               Seconds = duration.Seconds
+            };
+        }
+
+        // operator (--) overloading method
+       
+        public static Duration operator --(Duration duration)
+        {
+            return new Duration()
+            {
+                Hours = duration.Hours,
+                
+                Minutes = duration.Minutes - 1,
+                Seconds = duration.Seconds
+            };
+        }
+
+
+
+        // >
+        public static bool operator >(Duration d1, Duration d2)
+        {
+
+            return d1.ToTotalSeconds() > d2.ToTotalSeconds();
+        }
+
+
+        // < 
+        public static bool operator <(Duration d1, Duration d2)
+        {
+
+            return d1.ToTotalSeconds() < d2.ToTotalSeconds();
+        }
+
+        // >=
+        public static bool operator >=(Duration d1, Duration d2)
+        {
+       
+
+            return d1.ToTotalSeconds() >= d2.ToTotalSeconds();
+        }
+
+        // <=
+        public static bool operator <=(Duration d1, Duration d2)
+        {
+         
+
+            return d1.ToTotalSeconds() <= d2.ToTotalSeconds();
+        }
+
+
+         // true, false
+        public static implicit operator bool(Duration d)
+        {
+            return d.ToTotalSeconds() > 0;
+        }
+
+
+
+        public static explicit operator DateTime(Duration d)
+        {            
+            DateTime referenceDate = new DateTime();
+           
+            DateTime resultDate = referenceDate.AddSeconds(d.ToTotalSeconds());
+
+            return resultDate;
+        }
+
+
+    }
+}
